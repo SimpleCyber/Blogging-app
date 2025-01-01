659fda5845d681230613a59b7877035e67dc6dfd
Satyam Yadav <satyamok03@gmail.com>
2025-01-02 01:10:32 +0530

    staticfiles are not reccmonded to upload on github

--- usersproject/settings.py
+++ usersproject/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -147,3 +148,4 @@ CSRF_TRUSTED_ORIGINS = [
     "https://bloggersatyam.onrender.com",
 ]
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
